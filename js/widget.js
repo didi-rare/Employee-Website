4c18b20a4e95b82d011f8bb2d3b00c6f7a83a043
didi-rare <didi.pepple@gmail.com>
2017-02-10 17:52:55 +0100

    added via upload
    initial commit

---
+++ js/widget.js
@@ -0,0 +1,43 @@
+var emplxhr = new XMLHttpRequest();
+emplxhr.onreadystatechange = function () {
+    if (emplxhr.readyState === 4){
+        var employees = JSON.parse(emplxhr.responseText);
+        var StatusHTML = '<ul class="bulleted">';
+        for (var i=0; i<employees.length; i+=1){
+            if (employees[i].inoffice === true){
+                StatusHTML += '<li class="in">';
+            } else {
+                StatusHTML += '<li class="out">';
+            }
+            StatusHTML += employees[i].name;
+            StatusHTML += '</li>';
+        }
+        StatusHTML += '</ul>';
+        document.getElementById('employeeList').innerHTML = StatusHTML;
+    }
+};
+emplxhr.open('GET', 'data/employees.json');
+emplxhr.send();
+
+var statusxhr = new XMLHttpRequest();
+statusxhr.onreadystatechange = function () {
+    console.log(statusxhr.responseText);
+    if (statusxhr.readyState === 4){
+        var stat = JSON.parse(statusxhr.responseText);
+        var StatusHTML = '<ul class="rooms">';
+        for (var i=0; i<stat.length; i+=1){
+            if (stat[i].available === true){
+                StatusHTML += '<li class="empty">';
+            }else {
+                StatusHTML += '<li class="full">';
+            }
+            StatusHTML += stat[i].room;
+            StatusHTML += '</li>';
+        }
+        StatusHTML +='</ul>';
+        document.getElementById('roomList').innerHTML = StatusHTML;
+    }
+
+};
+statusxhr.open('Get', 'data/room.json');
+statusxhr.send();
