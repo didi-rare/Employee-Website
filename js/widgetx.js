7c375c9c3114e3e185439a55a4a28a96ceb52b4c
Didi Pepple <didi.pepple@gmail.com>
2017-02-11 01:06:27 +0100

    successfully used jquery to load json objects into html tag elemts

---
+++ js/widgetx.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+    var url_emply = "data/employees.json";
+    $.getJSON(url_emply, function (response) {
+        console.log(response);
+        var StatusHTML = '<ul class="bulleted">';
+        $.each(response, function (index, employee) {
+            if (employee.inoffice === true){
+                StatusHTML += '<li class="in">';
+            }else {
+                StatusHTML += '<li class="out">';
+            }
+            StatusHTML += employee.name + '</li>';
+        });//end of each
+        StatusHTML += '</ul>';
+        $('#employeeList').html(StatusHTML);
+    });//end of json
+
+    var url_room = "data/rooms.json";
+    $.getJSON(url_room, function (response) {
+        console.log(response);
+        var StatusHTML = '<ul class="rooms">';
+        $.each(response, function (index, room) {
+            if (room.available === true){
+                StatusHTML += '<li class="empty">';
+            }else {
+                StatusHTML += '<li class="full">';
+            }
+            StatusHTML += room.room + '</li>';
+        });//end of each
+        StatusHTML += '</ul>';
+        $('#roomList').html(StatusHTML);
+    });//end of json
+
+});//end of ready
